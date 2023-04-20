a57738564834f9124a9055348abf64724931315d
fabiodelli <ntqdel@hotmail.com>
2023-04-20 17:32:50 +0200

    add flag to store

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -1,31 +1,36 @@
 <script>
 import { store } from '../store.js'
 export default {
-  name: 'appMain',
-  data(){
-    return {
-      store
+    name: 'appMain',
+    data() {
+        return {
+            store
+        }
+    },
+    mounted() {
+        setInterval(() => {
+            console.log(store.flag);
+        }, 5000);
     }
-  },
 }
 </script>
 
 <template>
-  <h1>Boolflix</h1>
-  <input type="text" placeholder="Search film or series" v-model="text">
-  <input type="button" value="Search" @click='callApi(`${API_URL}&query=${text}`)'>
+    <h1>Boolflix</h1>
+    <input type="text" placeholder="Search film or series" v-model="store.text">
+    <input type="button" value="Search" @click='store.callApi(`${store.API_URL}&query=${store.text}`)'>
 
-  <div class="container d-flex flex-wrap">
-    <div v-for="(result, index) in store.apiRes" class="cards bg-info w-25 p-2">
-      <div>{{ result.title }}</div>
-      <div>{{ result.original_title }}</div>
-      <div>{{ result.original_language }}</div>
-      <div>{{ result.vote_average }}</div>
+    <div class="container d-flex flex-wrap">
+        <div v-for="(result, index) in store.apiRes" class="cards bg-info w-25 p-2">
+            <div>{{ result.title }}</div>
+            <div>{{ result.original_title }}</div>
+            <div>{{ result.original_language }}</div>
+            <img src="/{{ store.flag[index - 1] }}.png" alt="">
+            <img src="`/${{ store.flag[index] }}.png`" alt="">
+            <div>{{ result.vote_average }}</div>
 
+        </div>
     </div>
-  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/store.js
+++ src/store.js
@@ -2,23 +2,26 @@ import { reactive } from 'vue';
 import axios from 'axios'
 export const store = reactive({
 
-    API_URL: 'https://api.themoviedb.org/3/movie/550?api_key=4db66c26f4dc5d9d03f395cf284c2c0e',
+    API_URL: 'https://api.themoviedb.org/3/search/movie?api_key=4db66c26f4dc5d9d03f395cf284c2c0e',
     apiRes: null,
     text: '',
-    flag: '',
+    flag: [],
+    callApi(url) {
+        console.log(url)
+        axios
+            .get(url)
+            .then(response => {
+                this.apiRes = response.data.results
+                /* console.log(response.data.results[].original_language); */
+                this.apiRes.forEach((result, index) => {
+                    this.flag.push(result.original_language)
+                });
+                console.log(this.flag);
+            })
+            .catch(err => {
+                console.error(err.message);
+            })
+    }
 
-    
-        callApi(url) {
-            axios
-                .get(url)
-                .then(response => {
-                    this.apiRes = response.data.results
-                    console.log(this.flag);
-                })
-                .catch(err => {
-                    console.error(err.message);
-                })
-        }
-    
 
 })
